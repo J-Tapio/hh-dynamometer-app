0120ec797073521c85732ab06e8c6905ab8682b9
J-Tapio <skeletorcodes@gmail.com>
2022-11-14 13:38:53 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.digital_dynamometer',
+  appName: 'digital_dynamometer',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
